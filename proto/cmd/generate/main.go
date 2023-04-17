ab102cc91e48cfe7320a87a59d12ce62581425ae
earlgray <earlgray283@gmail.com>
2023-04-17 23:49:47 +0900

    fix: generate

---
+++ proto/cmd/generate/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"gopkg.in/yaml.v3"
+)
+
+const servicesRoot = "services"
+
+type LangYaml struct {
+	Dist   string   `yaml:"dist"`
+	Opts   []string `yaml:"opts"`
+	Plugin *struct {
+		Name string `yaml:"name"`
+		Path string `yaml:"path"`
+	} `yaml:"plugin"`
+}
+
+func (l *LangYaml) Options() []protocOptFunc {
+	opts := make([]protocOptFunc, 0)
+	opts = append(opts, optGrpcOut(l.Dist), optOut(l.Dist))
+	if l.Plugin != nil {
+		opts = append(opts, optPlugin(l.Plugin.Name, l.Plugin.Path))
+	}
+	for _, opt := range l.Opts {
+		opts = append(opts, optGrpcOpt(opt), optOpt(opt))
+	}
+	return opts
+}
+
+func main() {
+	serviceEntries, err := os.ReadDir(servicesRoot)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, serviceEntry := range serviceEntries {
+		serviceYaml, protoFiles, err := loadService(serviceEntry.Name())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, protoFile := range protoFiles {
+			for langName, lang := range serviceYaml {
+				options := lang.Options()
+				if err := runProtoc(langName, protoFile, options...); err != nil {
+					log.Fatal(err)
+				}
+			}
+		}
+
+	}
+}
+
+func loadService(serviceName string) (map[string]LangYaml, []string, error) {
+	serviceDirPath := filepath.Join("services", serviceName)
+	f, err := os.Open(filepath.Join(serviceDirPath, "service.yaml"))
+	if err != nil {
+		return nil, nil, err
+	}
+	defer f.Close()
+	var serviceYaml map[string]LangYaml
+	if err := yaml.NewDecoder(f).Decode(&serviceYaml); err != nil {
+		return nil, nil, err
+	}
+
+	protoFiles, err := os.ReadDir(serviceDirPath)
+	if err != nil {
+		return nil, nil, err
+	}
+	protoFileList := make([]string, 0)
+	for _, protoFile := range protoFiles {
+		if !strings.HasSuffix(protoFile.Name(), ".proto") {
+			continue
+		}
+		protoFileList = append(protoFileList, filepath.Join(serviceDirPath, protoFile.Name()))
+	}
+	return serviceYaml, protoFileList, nil
+}
