362590bb483f93c3396eb8025d19410d94565348
earlgray <earlgray283@gmail.com>
2023-04-20 00:42:24 +0900

    feat: add jwt secret

--- backend/server/core/config/config.go
+++ backend/server/core/config/config.go
@@ -3,12 +3,13 @@ package config
 import "github.com/kelseyhightower/envconfig"
 
 type Config struct {
-	DBUser   string `envconfig:"DB_USER"`
-	DBPass   string `envconfig:"DB_PASS"`
-	DBHost   string `envconfig:"DB_HOST"`
-	DBPort   string `envconfig:"DB_PORT"`
-	DBName   string `envconfig:"DB_NAME"`
-	GrpcPort string `envconfig:"GRPC_PORT" default:"50051"`
+	DBUser    string `envconfig:"DB_USER" required:"true"`
+	DBPass    string `envconfig:"DB_PASS" required:"true"`
+	DBHost    string `envconfig:"DB_HOST" required:"true"`
+	DBPort    string `envconfig:"DB_PORT" required:"true"`
+	DBName    string `envconfig:"DB_NAME" required:"true"`
+	GrpcPort  string `envconfig:"GRPC_PORT" default:"50051"`
+	JwtSecret string `envconfig:"JWT_SECRET" required:"true"`
 }
 
 func New() (*Config, error) {
