6a5657823bd7adc47c445697ff62d0352133a72b
earlgray <earlgray283@gmail.com>
2023-04-19 17:34:07 +0900

    feat: add grpc methods(unimplemented)

--- backend/server/interfaces/grpc/backend_service.go
+++ backend/server/interfaces/grpc/backend_service.go
@@ -3,17 +3,32 @@ package grpc
 import (
 	"context"
 
-	"github.com/ohkilab/SU-CSexpA-benchmark-system/proto-gen/go/backend"
+	pb "github.com/ohkilab/SU-CSexpA-benchmark-system/proto-gen/go/backend"
 )
 
 type backendServiceServer struct {
 }
 
-func newBackendService() backend.BackendServiceServer {
+func newBackendService() pb.BackendServiceServer {
 	return &backendServiceServer{}
 }
 
-func (s *backendServiceServer) GetRanking(ctx context.Context, req *backend.GetRankingRequest) (*backend.GetRankingResponse, error) {
+func (s *backendServiceServer) GetRanking(ctx context.Context, req *pb.GetRankingRequest) (*pb.GetRankingResponse, error) {
+	// !!!unimplemented!!!
+	return nil, nil
+}
+
+func (s *backendServiceServer) PostSubmit(ctx context.Context, req *pb.PostSubmitRequest) (*pb.PostSubmitResponse, error) {
+	// !!!unimplemented!!!
+	return nil, nil
+}
+
+func (s *backendServiceServer) GetSubmit(req *pb.GetSubmitRequest, stream pb.BackendService_GetSubmitServer) error {
+	// !!!unimplemented!!!
+	return nil
+}
+
+func (s *backendServiceServer) PostLogin(ctx context.Context, req *pb.PostLoginRequest) (*pb.PostLoginResponse, error) {
 	// !!!unimplemented!!!
 	return nil, nil
 }
