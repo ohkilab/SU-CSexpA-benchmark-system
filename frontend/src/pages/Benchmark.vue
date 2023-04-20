7632ad6cdd82561926a6da9909776eaf6c8fce67
Naborisk <github@naborisk.com>
2023-04-21 01:57:36 +0900

    add benchmark button

--- frontend/src/pages/Benchmark.vue
+++ frontend/src/pages/Benchmark.vue
@@ -3,11 +3,13 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { onMounted, reactive } from 'vue'
 
 interface Status {
+  benchmarking: boolean,
   current: number
   size: number
 }
 
 const status: Status = reactive({
+  benchmarking: false,
   current: 0,
   size: 20
 })
@@ -20,20 +22,27 @@ onMounted(() => {
 })
 </script>
 <template>
-  <div class="flex flex-col gap-8 my-auto items-center justify-center">
-    <div class="p-3 bg-gray-700 rounded shadow-md shadow-black opacity-70 scale-90">
-      <font-awesome-icon :icon="['fas', 'check']" />
-      Benchmarking 1/20 tags
+  <div class="flex flex-col my-auto">
+    <div v-if="status.benchmarking">
+      <div class="flex flex-col gap-8 my-auto items-center justify-center">
+        <div class="p-3 bg-gray-700 rounded shadow-md shadow-black opacity-70 scale-90">
+          <font-awesome-icon :icon="['fas', 'check']" />
+          Benchmarking 1/20 tags
+        </div>
+        <hr class="h-[2px] w-11/12 mx-8 text-white bg-gray-500 border-0" />
+        <div class="p-3 bg-gray-700 rounded shadow-md shadow-black">
+          <font-awesome-icon class="animate-spin" :icon="['fas', 'spinner']" />
+          Benchmarking {{ `${status.current}/${status.size}` }} tags
+        </div>
+        <hr class="h-[2px] w-11/12 mx-8 text-white bg-gray-500 border-0" />
+        <div class="p-3 bg-gray-700 rounded shadow-md shadow-black opacity-70 scale-90">
+          <font-awesome-icon :icon="['fas', 'check']" />
+          Benchmarking 0/20 tags
+        </div>
+      </div>
     </div>
-    <hr class="h-[2px] w-11/12 mx-8 text-white bg-gray-500 border-0" />
-    <div class="p-3 bg-gray-700 rounded shadow-md shadow-black">
-      <font-awesome-icon class="animate-spin" :icon="['fas', 'spinner']" />
-      Benchmarking {{ `${status.current}/${status.size}` }} tags
-    </div>
-    <hr class="h-[2px] w-11/12 mx-8 text-white bg-gray-500 border-0" />
-    <div class="p-3 bg-gray-700 rounded shadow-md shadow-black opacity-70 scale-90">
-      <font-awesome-icon :icon="['fas', 'check']" />
-      Benchmarking 0/20 tags
+    <div v-else>
+      <button class="p-5 bg-blue-500 rounded shadow-md shadow-black hover:scale-105 transition" @click="status.benchmarking = true">ベンチマーク開始</button>
     </div>
   </div>
 </template>
