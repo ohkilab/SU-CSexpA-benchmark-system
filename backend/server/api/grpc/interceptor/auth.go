3fa38e980f631dbbaf420bd861bd4e907d8806db
earlgray <earlgray283@gmail.com>
2023-04-20 15:47:35 +0900

    fix(interceptor): ignore authentication in the case of specify method

--- backend/server/api/grpc/interceptor/auth.go
+++ backend/server/api/grpc/interceptor/auth.go
@@ -5,16 +5,29 @@ import (
 
 	grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/auth"
 	"github.com/ohkilab/SU-CSexpA-benchmark-system/backend/server/core/auth"
+	"github.com/ohkilab/SU-CSexpA-benchmark-system/proto-gen/go/backend"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
 var claimsKey struct{}
+var excludeMethodSet = map[string]struct{}{
+	backend.BackendService_PostLogin_FullMethodName: {},
+}
 
 // authentication with JWT
 func Auth(secret string) grpc.UnaryServerInterceptor {
 	return grpc_auth.UnaryServerInterceptor(func(ctx context.Context) (context.Context, error) {
+		method, ok := grpc.Method(ctx)
+		if !ok {
+			return nil, status.Error(codes.InvalidArgument, "failed to get method name from context")
+		}
+		// skip authentication
+		if _, ok := excludeMethodSet[method]; ok {
+			return ctx, nil
+		}
+
 		token, err := grpc_auth.AuthFromMD(ctx, "bearer")
 		if err != nil {
 			return nil, err
