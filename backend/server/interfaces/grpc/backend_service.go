a09ce663e1953c688cf62d7e36f402ade47d45e4
earlgray <earlgray283@gmail.com>
2023-04-29 18:05:39 +0900

    feat(backend): GetRanking

--- backend/server/interfaces/grpc/backend_service.go
+++ backend/server/interfaces/grpc/backend_service.go
@@ -5,21 +5,27 @@ import (
 
 	"github.com/ohkilab/SU-CSexpA-benchmark-system/backend/ent"
 	"github.com/ohkilab/SU-CSexpA-benchmark-system/backend/server/usecases/auth"
+	"github.com/ohkilab/SU-CSexpA-benchmark-system/backend/server/usecases/ranking"
 	pb "github.com/ohkilab/SU-CSexpA-benchmark-system/proto-gen/go/backend"
 )
 
 type backendServiceServer struct {
-	authInteractor *auth.AuthInteractor
+	authInteractor    *auth.AuthInteractor
+	rankingInteractor *ranking.RankingInteractor
 }
 
 func NewBackendService(secret string, entClient *ent.Client) pb.BackendServiceServer {
 	authInteractor := auth.NewInteractor(secret, entClient)
-	return &backendServiceServer{authInteractor}
+	rankingInteractor := ranking.NewInteractor(entClient)
+	return &backendServiceServer{authInteractor, rankingInteractor}
 }
 
 func (s *backendServiceServer) GetRanking(ctx context.Context, req *pb.GetRankingRequest) (*pb.GetRankingResponse, error) {
-	// !!!unimplemented!!!
-	return nil, nil
+	records, err := s.rankingInteractor.GetRanking(ctx, req.ContainGuest, int(req.Year))
+	if err != nil {
+		return nil, err
+	}
+	return &pb.GetRankingResponse{Records: records}, nil
 }
 
 func (s *backendServiceServer) PostSubmit(ctx context.Context, req *pb.PostSubmitRequest) (*pb.PostSubmitResponse, error) {
