4952d3de249dfbb7932fb79d1dc2c9798f2d09be
Naborisk <github@naborisk.com>
2023-05-03 19:39:12 +0900

    fix ranking highlight

--- frontend/src/components/RankItem.vue
+++ frontend/src/components/RankItem.vue
@@ -2,7 +2,7 @@
 defineProps<{ rank: number; score: number; name: string }>()
 </script>
 <template>
-  <div class="cursor-pointer bg-gray-700 rounded shadow-gray-950 shadow-md flex gap-4 px-2 py-4 w-full sm:w-[400px] justify-around hover:scale-105 transition">
+  <div class="cursor-pointer rounded shadow-gray-950 shadow-md flex gap-4 px-2 py-4 w-full sm:w-[400px] justify-around hover:scale-105 transition">
   <div class="w-8 text-center justify-center">
     {{ rank }}
   </div>
