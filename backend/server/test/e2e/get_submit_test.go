37193c27368e41f806621baf858ba434baa6c62e
earlgray <earlgray283@gmail.com>
2023-05-05 19:50:23 +0900

    test(e2e): GetSubmit

--- backend/server/test/e2e/get_submit_test.go
+++ backend/server/test/e2e/get_submit_test.go
@@ -8,17 +8,24 @@ import (
 
 	"github.com/ohkilab/SU-CSexpA-benchmark-system/backend/ent/group"
 	"github.com/ohkilab/SU-CSexpA-benchmark-system/backend/server/api/grpc"
-	"github.com/ohkilab/SU-CSexpA-benchmark-system/backend/server/core/timejst"
+	"github.com/ohkilab/SU-CSexpA-benchmark-system/backend/server/core/auth"
 	"github.com/ohkilab/SU-CSexpA-benchmark-system/backend/server/test/utils"
+	"github.com/ohkilab/SU-CSexpA-benchmark-system/backend/worker"
 	pb "github.com/ohkilab/SU-CSexpA-benchmark-system/proto-gen/go/backend"
+	benchmarkpb "github.com/ohkilab/SU-CSexpA-benchmark-system/proto-gen/go/benchmark"
 	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc/metadata"
 )
 
 func Test_GetSubmit(t *testing.T) {
 	ctx := context.Background()
 	entClient, cleanupFunc := utils.EnttestOpen(ctx, t)
 	defer cleanupFunc(t)
-	conn, closeFunc := utils.LaunchGrpcServer(t, grpc.WithEntClient(entClient))
+	connBenchmark, closeBenchmark := utils.LaunchBenchmarkGrpcServer(t)
+	defer closeBenchmark()
+	benchmarkClient := benchmarkpb.NewBenchmarkServiceClient(connBenchmark)
+	worker := worker.New(entClient, benchmarkClient)
+	conn, closeFunc := utils.LaunchGrpcServer(t, grpc.WithEntClient(entClient), grpc.WithJwtSecret("secret"), grpc.WithWorker(worker))
 	defer closeFunc()
 	client := pb.NewBackendServiceClient(conn)
 
@@ -30,16 +37,29 @@ func Test_GetSubmit(t *testing.T) {
 		SetScore(12345).
 		SetYear(2023).
 		Save(ctx)
-	submit, _ := entClient.Submit.Create().
-		SetIPAddr("10.255.255.255").
+	contest, _ := entClient.Contest.Create().
+		SetTitle("test contest").
+		SetStartAt(time.Date(2023, time.January, 1, 0, 0, 0, 0, time.UTC)).
+		SetEndAt(time.Date(2023, time.December, 31, 23, 59, 59, 0, time.UTC)).
 		SetYear(2023).
-		SetScore(100).
-		SetSubmitedAt(timejst.Now()).
-		SetGroups(group).
-		SetCompletedAt(timejst.Now()).
+		SetSubmitLimit(9999).
+		SetCreatedAt(time.Now()).
 		Save(ctx)
 
-	stream, err := client.GetSubmit(ctx, &pb.GetSubmitRequest{SubmitId: int32(submit.ID)})
+	jwtToken, err := auth.GenerateJWTToken([]byte("secret"), group.ID, group.Year)
+	if err != nil {
+		t.Fatal(err)
+	}
+	meta := metadata.New(map[string]string{"authorization": "Bearer " + jwtToken})
+	ctx = metadata.NewOutgoingContext(ctx, meta)
+	submit, err := client.PostSubmit(ctx, &pb.PostSubmitRequest{
+		IpAddr:    "10.255.255.255",
+		ContestId: int32(contest.ID),
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	stream, err := client.GetSubmit(ctx, &pb.GetSubmitRequest{SubmitId: int32(submit.Id)})
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -67,7 +87,10 @@ func Test_GetSubmit(t *testing.T) {
 	}
 	assert.True(t, ok)
 
-	submit, _ = entClient.Submit.Get(ctx, submit.ID)
-	assert.NotEmpty(t, submit.Score)
-	assert.NotNil(t, submit.CompletedAt)
+	dbSubmit, err := entClient.Submit.Get(ctx, int(submit.Id))
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.NotEmpty(t, dbSubmit.Score)
+	assert.NotNil(t, dbSubmit.CompletedAt)
 }
