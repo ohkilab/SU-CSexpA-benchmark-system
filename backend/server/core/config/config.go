9030963a176de0688af7bd2ae9d558f3a41b9c76
earlgray <earlgray283@gmail.com>
2023-05-14 00:33:57 +0900

    fix(compose): 諸々

--- backend/server/core/config/config.go
+++ backend/server/core/config/config.go
@@ -9,6 +9,7 @@ type Config struct {
 	DBPort        string `envconfig:"BACKEND_MYSQL_PORT" required:"true"`
 	DBName        string `envconfig:"MYSQL_DATABASE" required:"true"`
 	BenchmarkHost string `envconfig:"BACKEND_BENCHMARK_HOST" required:"true"`
+	BenchmarkPort string `envconfig:"BENCHMARK_GRPC_PORT" required:"true"`
 	GrpcPort      string `envconfig:"BACKEND_GRPC_PORT" default:"50051"`
 	JwtSecret     string `envconfig:"BACKEND_JWT_SECRET" required:"true"`
 }
