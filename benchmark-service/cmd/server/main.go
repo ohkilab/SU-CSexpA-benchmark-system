a465a04ce57c5e4fd08c5daf31b42fa8aa5a39da
earlgray <earlgray283@gmail.com>
2023-06-04 15:44:00 +0900

    fix(benchmark): add reflection

--- benchmark-service/cmd/server/main.go
+++ benchmark-service/cmd/server/main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/ohkilab/SU-CSexpA-benchmark-system/benchmark-service/service"
 	pb "github.com/ohkilab/SU-CSexpA-benchmark-system/proto-gen/go/benchmark"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
@@ -26,6 +27,7 @@ func main() {
 
 	grpcServer := grpc.NewServer()
 	benchmarkService := service.New(client)
+	reflection.Register(grpcServer)
 	grpcServer.RegisterService(&pb.BenchmarkService_ServiceDesc, benchmarkService)
 
 	if err := grpcServer.Serve(lsnr); err != nil {
