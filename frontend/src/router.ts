71fd8a876581256933a0819e0529f54296a4649f
Naborisk <github@naborisk.com>
2023-06-10 03:21:22 +0900

    fix build errors in router.ts

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -8,8 +8,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
   {
     path: '/',
-    component: Loading,
-    redirect: '/benchmark'
+    component: Loading
   },
   {
     path: '/login',
