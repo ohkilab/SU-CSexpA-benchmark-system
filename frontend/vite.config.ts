7d3f8bdc839e98c88cd66b4fcfd0cfb8497b4cd4
Naborisk <github@naborisk.com>
2024-06-09 04:38:46 +0900

    separate compose-dev.yaml and compose-image.yaml

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [vue()],
+    server: {
+      host: "0.0.0.0",
+      hmr: {
+        protocol: "ws",
+        port: env.HMR_WS_PORT
+      },
+      port: env.INNER_PORT_FRONTEND,
+    },
+  };
 });
